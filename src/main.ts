357024d9890e7ecebe575e2d9eb225c1df3db0ed
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-09-06 16:37:05 -0300

    Prisma setup

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  console.log("Port is running at:", process.env.PORT ?? 3000)
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+
+import { Injectable } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class PrsimaService extends PrismaClient {
+  public client: PrismaClient
+
+  constructor() {
+    super({
+      log: ['warn', 'error']
+    })
+  }
+
+  onModuleInit() {
+    return this.$connect()
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
