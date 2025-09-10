fd8d4b6255212dc247a866e9455381e36885d708
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-09-10 00:13:39 -0300

    Apply eslint to code

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,11 @@
-import { Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { JwtModule } from "@nestjs/jwt";
-import { PassportModule } from "@nestjs/passport";
-import { AuthenticateController } from "src/controllers/authenticate-controller";
-import { Env } from "src/env";
-import { PrismaModule } from "src/prisma/prisma.module";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { AuthenticateController } from 'src/controllers/authenticate-controller'
+import { Env } from 'src/env'
+import { PrismaModule } from 'src/prisma/prisma.module'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Module({
   imports: [
@@ -15,26 +15,25 @@ import { PrismaService } from "src/prisma/prisma.service";
       inject: [ConfigService],
       global: true,
       useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true });
-        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
+        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
 
         if (!privateKey) {
-          throw new Error('JWT_PRIVATE_KEY is not defined');
+          throw new Error('JWT_PRIVATE_KEY is not defined')
         }
         if (!publicKey) {
-          throw new Error('JWT_PUBLIC_KEY is not defined');
+          throw new Error('JWT_PUBLIC_KEY is not defined')
         }
 
         return {
           signOptions: { algorithm: 'RS256' },
-          privateKey: Buffer.from(privateKey  , 'base64'),
+          privateKey: Buffer.from(privateKey, 'base64'),
           publicKey: Buffer.from(publicKey, 'base64'),
         }
-      }
+      },
     }),
   ],
   controllers: [AuthenticateController],
   providers: [PrismaService],
 })
-
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ConfigService } from '@nestjs/config';
-import { Env } from './env';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ConfigService } from '@nestjs/config'
+import { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -12,7 +12,10 @@ async function bootstrap() {
     .setDescription('Complete description about FastFeet API')
     .setVersion('1.0')
     .addBearerAuth()
-    .build();
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api/docs', app, document)
 
   const configService = app.get<ConfigService<Env, true>>(ConfigService)
   const port = configService.get('PORT', { infer: true })
